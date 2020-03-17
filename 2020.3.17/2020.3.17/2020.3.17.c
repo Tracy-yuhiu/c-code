90856d9f7425602eb2d0b2dde1b11c23c59aeb18
Tracy-yuhui <1135196191@qq.com>
2020-03-17 20:33:04 +0800

    提交代码

---
+++ 2020.3.17/2020.3.17/2020.3.17.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char arr[] = { 'b','i','t' };
+	char ch[10] = "hello bit";
+	printf("%d\n", strlen("c:\test\121"));
+	printf("%s\n", ch);
+	return 0;
+}
